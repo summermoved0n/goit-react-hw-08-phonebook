f96bbc9c03330b904cf712c7c74b30e6c372ace4
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-07-25 11:30:41 -0400

    start of changes

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,7 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+axios.defaults.baseURL = 'https://connections-api.goit.global';
 
 const token = {
   set(token) {
--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,21 +1,18 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.goit.global';
 
 export async function getFetchContacts() {
   const { data } = await axios.get('/contacts');
-  console.log(data);
   return data;
 }
 
 export async function postFetchContact(postData) {
   const { data } = await axios.post('/contacts', postData);
-  console.log(data);
   return data;
 }
 
 export async function deleteFetchContact(id) {
   const { data } = await axios.delete(`/contacts/${id}`);
-  console.log(data);
   return data;
 }
